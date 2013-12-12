2dbf9127938d2af76c7bd61511404de7f407230a
javier Miras <jmirasb@gmail.com>
2013-12-12 19:12:26 +0100

    vmdk export script more quiet

--- scripts/export_vmdk.sh
+++ scripts/export_vmdk.sh
@@ -23,21 +23,20 @@ echo "INFO: Export opendomo to RAW image ..."
 rm $EXPORTDIR/$IMGNAME.img 2>/dev/null
 
 # Creating image
-qemu-img create -f raw $EXPORTDIR/$IMGNAME.img $1
-ls $EXPORTDIR
+qemu-img create -f raw $EXPORTDIR/$IMGNAME.img $1 >/dev/null
 
 # Creating RAW image
 losetup -f $EXPORTDIR/$IMGNAME.img
 LOOPDEV=`losetup -a | grep -m1 "$EXPORTDIR/$IMGNAME.img" | cut -f1 -d:`
 
 if
-mkfs.vfat $LOOPDEV
+mkfs.vfat $LOOPDEV >/dev/null 2>/dev/null
 then
 	mount $EXPORTDIR/$IMGNAME.img $MOUNTDIR
 	cp -rp $IMAGEDIR/* $MOUNTDIR/
 
 	# Installing bootloader
-	extlinux -i $MOUNTDIR
+	extlinux -i $MOUNTDIR >/dev/null 2>/dev/null
 
 	# Wait for unmount
 	sleep 5
@@ -46,8 +45,8 @@ then
 fi
 
 # Convert raw to vmdk
-qemu-img convert -O vmdk $EXPORTDIR/$IMGNAME.img $EXPORTDIR/$IMGNAME.vmdk
+qemu-img convert -O vmdk $EXPORTDIR/$IMGNAME.img $EXPORTDIR/$IMGNAME.vmdk >/dev/null
 
 # Fix image perms
-chown $USER $EXPORTDIR/$IMGNAME.img
-chown $USER $EXPORTDIR/$IMGNAME.vmdk
+chown $USER $EXPORTDIR/$IMGNAME.img 2>/dev/null
+chown $USER $EXPORTDIR/$IMGNAME.vmdk 2>/dev/null
