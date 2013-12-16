eb0fe04088455fc19b61f3445d67935f1be721e4
javier Miras <jmirasb@gmail.com>
2013-12-16 17:57:31 +0100

    Updating default configurations

---
+++ files/sysconf/etc/opendomo/login.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "#> Welcome to OpenDomo"
+echo "form:login.sh	loginform"
+echo "	USER	User name	input	$1"
+echo "	PASS	Password	password"
+echo "actions:"
+echo "	login.sh	Keyboard	keyboard"
+echo "	login.sh	Advanced	advanced"
+echo "	login.sh	Login	login"
+echo
