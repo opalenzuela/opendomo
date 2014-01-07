cd0a8cbcdd3e58ac4ff3e7ab3ee0884886102d22
opalenzuela <opalenzuela@gmail.com>
2014-01-07 10:26:26 +0100

    Logic was inverted in managePlugins

--- src/odcommon/usr/local/opendomo/services/config/managePlugins.sh
+++ src/odcommon/usr/local/opendomo/services/config/managePlugins.sh
@@ -7,50 +7,49 @@ OSVER=`cat /etc/VERSION`
 OSVER="2.0.0"
 REPOSITORY="http://cloud.opendomo.com/repo/$OSVER/"
 if ! test -f $TMPDIR/repo.lst; then
-    if wget $REPOSITORY -O $TMPDIR/repo.lst --no-check-certificate --max-redirect=0 2>/dev/null
-    then
-        echo "#INFO Repository updated"
-    else
-        echo "#ERROR Cannot find repository for version [$OSVER]"
-        exit 1
-    fi
+	if wget $REPOSITORY -O $TMPDIR/repo.lst --no-check-certificate --max-redirect=0 2>/dev/null
+	then
+		echo "#INFO Repository updated"
+	else
+		echo "#ERROR Cannot find repository for version [$OSVER]"
+		exit 1
+	fi
 fi
 if test -z "$1"; then
-
-    echo "#> Manage plugins"
-    echo  "list:managePlugins.sh	iconlist"
-    for p in `grep -v "#" $TMPDIR/repo.lst | cut -f1 -d- | uniq`; do
+	echo "#> Manage plugins"
+	echo  "list:managePlugins.sh	iconlist"
+	for p in `grep -v "#" $TMPDIR/repo.lst | cut -f1 -d- | uniq`; do
 	ID=`grep $p $TMPDIR/repo.lst | cut -f1 -d'-' | head -n1`
 	DESC=`grep $p $TMPDIR/repo.lst | cut -f3 -d';' | head -n1`
 	echo "	-$ID	$DESC	plugin"
-    done
-    if test -z "$ID"; then
+	done
+	if test -z "$ID"; then
 	echo "#ERROR The repository was empty. Try again later"
-    fi
-    echo
+	fi
+	echo
 
 #echo "actions:"
 #echo "   managePlugins.sh   Details"
 #echo
 
 else
-    # Parameter was passed (requesting plugin's details)
-    URL=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f2 -d';' `
-    DESC=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f3 -d';' `
-    DEPS=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f4 -d';' `
-    if test -z "$DEPS"; then
-	DEPS="none"
-    fi
-    echo "#> Plugin details"
-    echo "form:managePlugins.sh"
-    echo "	code	Code	$1	readonly"
-    echo "	desc	Description	$DESC	readonly"
-    echo "	deps	Dependences	$DEPS	readonly"
-    echo "	icon	Icon	$1.png	image"
-    echo "actions:"
-    if test -f /var/opendomo/plugins/$1; then
-	echo "	installPlugin.sh	Install"
-    else
-	echo "	removePlugin.sh	Uninstall"
-    fi
+	# Parameter was passed (requesting plugin's details)
+	URL=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f2 -d';' `
+	DESC=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f3 -d';' `
+	DEPS=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f4 -d';' `
+	if test -z "$DEPS"; then
+		DEPS="none"
+	fi
+	echo "#> Plugin details"
+	echo "form:managePlugins.sh"
+	echo "	code	Code	$1	readonly"
+	echo "	desc	Description	$DESC	readonly"
+	echo "	deps	Dependences	$DEPS	readonly"
+	echo "	icon	Icon	$1.png	image"
+	echo "actions:"
+	if ! test -f /var/opendomo/plugins/$1; then
+		echo "	installPlugin.sh	Install"
+	else
+		echo "	removePlugin.sh	Uninstall"
+	fi
 fi
