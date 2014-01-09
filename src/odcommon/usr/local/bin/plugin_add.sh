20d524bf6db7d42e583cf0fbe4c3a371229f72a7
opalenzuela <opalenzuela@gmail.com>
2014-01-09 09:45:51 +0100

    First tests with plugin update scripts

--- src/odcommon/usr/local/bin/plugin_add.sh
+++ src/odcommon/usr/local/bin/plugin_add.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#desc:Install plugin file
+# This script will install the plugin file or exit with error codes.
+# It will ignore versions of the files, so it can be used to install an older
+# version than the installed one.
 
 if test -z "$1"
 then
@@ -10,12 +14,17 @@ fi
 
 if test -f "$1"
 then 
-    #If the parameter is a file
+    # If the parameter is a file, the name should be composed by PKGID-VERSION
     PKGID=`basename $1| cut -f1 -d'-'`
+	PKGVER=`basename $1| cut -f1 -d. | cut -f2 -d-`
 else
+	# otherwise, the version will be taken from the repository index
     PKGID="$1"
+	PKGVER=""
 fi
 
+# The repository index file must exist. 
+# Otherwise it will trigger a self-explanatory error.
 REPOFILE="/var/opendomo/tmp/repo.lst"
 if ! test -f "$REPOFILE"
 then
@@ -40,6 +49,7 @@ echo "00" > $PROGRESS
 if wget $URLFILE -O $LFILE -q --no-check-certificate
 then
     SHA1=`grep ^$1 $REPOFILE| cut -f5 -d';' |sort |head -n1`
+	PKGVER=`basename $LFILE| cut -f1 -d'.' | cut -f2 -d'-'`
     #TODO Check SHA1
 else
     exit 2
@@ -53,6 +63,9 @@ else
 	echo "No dependencies found"
 fi
 echo "75" > $PROGRESS
+
+#Saving timestamp
+echo $PKGVER > /var/opendomo/plugins/$PKGID.version
 
 cd /
 echo $LFILE | grep ".tar.gz" - && /bin/tar -zxvf $LFILE | grep -v /$ > /var/opendomo/plugins/$PKGID.files
---
+++ src/odcommon/usr/local/bin/plugin_update.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#desc:Update all plugin files
+# This script will update the installed plugins, downloading from the repository
+# only the newer files.
+
+if test -z "$1"
+then
+    echo "usage: $0"
+    echo
+    exit 1
+fi
+
+
+STORAGE="/mnt/odconf/plugins"
+if ! test -d $STORAGE
+then
+    mkdir -p $STORAGE
+fi
+
+# First we create a list with all the installed plugins
+cd /var/opendomo/plugins/
+for i in *.info
+do
+	f=`echo $i | cut -f1 -d'.'`
+	PLIST="$PLIST $f"
+done
+cd /
+
+echo "# Updating $PLIST ..."
+
+# The repository index file must exist. 
+# Otherwise it will trigger a self-explanatory error.
+REPOFILE="/var/opendomo/tmp/repo.lst"
+if ! test -f "$REPOFILE"
+then
+	echo "#ERROR $REPOFILE does not exist. Launch managePlugins.sh and try again"
+	exit 2
+fi
+for PKGID in $PLIST
+do
+	LVER=`cat /var/opendomo/plugins/$PKGID.version`
+	RVER=`grep ^$PKGID $REPOFILE| cut -f1 -d';' |sort |head -n1|cut -f1 -d. |cut -f2 -d-`
+	
+	if test -z "$RVER" || test -z "$LVER"
+	then
+		echo "# Missing version information for [$PKGID]:"
+		echo "# Remote: $RVER - Local: $LVER "
+	else
+		if test "$RVER" -gt "$LVER"
+		then
+			echo "# Remote version is newer"
+			/usr/local/bin/plugin_add.sh $PKGID
+		fi
+	fi
+
+done
