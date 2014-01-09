6fc65589c0791ecbfa099267b355d0010d1c4ecd
opalenzuela <opalenzuela@gmail.com>
2014-01-09 14:53:10 +0100

    recovering logevent script
    now belongs to odcommon

--- src/odcommon/usr/local/opendomo/services/syscript/logEvent.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/logEvent.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
-#desc:Recepción de eventos
-#package:odevents
+#desc:Event reception
+#package:odcommon
 #type:local
 
-# Copyright(c) 2011 OpenDomo Services SL. Licensed under GPL v3 or later
-
-# Este script recibe los eventos de la red OpenDomo con la siguiente estructura:
-# logEvent.sh [nivel] [modulo] [mensaje]
-# nivel=error|warn|notice|
-# modulo=odcgi|odpkg|oddiscovery|... cualquier modulo instalado
-# mensaje=Cadena de texto
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 EHDIR="/etc/opendomo/eventhandlers"
 
 if test -z "$1" || test -z "$2" || test -z "$3"
 then
-	echo "Usage: $0 nivel modulo mensaje [archivo]"
+	echo "Usage: $0 level module message [archivo]"
+	echo "	level=error|warn|notice|debug"
+	echo "	module=odcgi|odpkg|oddiscovery|any plugin or package installed"
+	echo "	message=description of the event"
 	exit 0
 fi
-
 
 LEVEL=$1
 MODULE=$2
@@ -28,5 +24,6 @@ test -x $EHDIR/all-all && . $EHDIR/all-all $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/$MODULE-all && . $EHDIR/$MODULE-all $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/all-$LEVEL && . $EHDIR/all-$LEVEL  $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/$MODULE-$LEVEL && . $EHDIR/$MODULE-$LEVEL $LEVEL $MODULE "$MSG" $4
+#TODO remove the following line in future versions:
 test -x $EHDIR/odai_rules-$MODULE-$LEVEL && . $EHDIR/$MODULE-$LEVEL $LEVEL $MODULE "$MSG" $4
 
