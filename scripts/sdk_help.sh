5141baff03c30b77caa5ba68bd633168518c00b9
opalenzuela <opalenzuela@gmail.com>
2014-02-03 09:35:27 +0100

    help format suitable for all terminals

--- scripts/sdk_help.sh
+++ scripts/sdk_help.sh
@@ -2,16 +2,16 @@
 # desc: Help for opendomo SDK
 
 cat <<- HELPEND
-	Welcome debian opendomo SDK
-	USAGE:
+Welcome to OpenDomo OS 2 SDK
+USAGE:
 
-	   ./sdk.sh build i386        - Build opendomo for i386 arch
-	   ./sdk.sh build arm         - Build opendomo for RaspberryPi
+  ./sdk.sh build i386        - Build opendomo for i386 arch
+  ./sdk.sh build arm         - Build opendomo for RaspberryPi
 
-	   ./sdk.sh export sd dev     - Export opendomo distro to SD card (i386 only)"
-	   ./sdk.sh export vmdk       - Export opendomo distro to VMDK image (i386 only)"
-	   ./sdk.sh export rpi dev    - Export opendomo distro to RaspberryPI SD card"
+  ./sdk.sh export sd dev     - Export opendomo distro to SD card (i386 only)
+  ./sdk.sh export vmdk       - Export opendomo distro to VMDK image (i386 only)
+  ./sdk.sh export rpi dev    - Export opendomo distro to RaspberryPI SD card
 
-	   ./sdk.sh clean             - Clean images and custom configs"
+  ./sdk.sh clean             - Clean images and custom configs
 
 HELPEND
