014c732d10f76761906370a2c48e812d047de418
opalenzuela <opalenzuela@gmail.com>
2014-03-04 16:12:39 +0100

    Adding plugin's URL and icon

--- src/odcommon/usr/local/opendomo/services/config/managePlugins.sh
+++ src/odcommon/usr/local/opendomo/services/config/managePlugins.sh
@@ -42,6 +42,7 @@ else
 	URL=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f2 -d';' `
 	DESC=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f3 -d';' `
 	DEPS=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f4 -d';' `
+	WEB=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f6 -d';' `
 	if test -z "$DEPS"; then
 		DEPS="none"
 	fi
@@ -50,7 +51,8 @@ else
 	echo "	code	Code	readonly	$1"
 	echo "	desc	Description	readonly	$DESC"
 	echo "	deps	Dependences	readonly	$DEPS"
-	echo "	icon	Icon	image	$1.png"
+	echo "	webp	Web page	readonly	$WEB"
+	echo "	icon	Icon	image	$WEB/$1.png"
 	echo "actions:"
 	if ! test -f /var/opendomo/plugins/$1.version; then
 		echo "	installPlugin.sh	Install"
