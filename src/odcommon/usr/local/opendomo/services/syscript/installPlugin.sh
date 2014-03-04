8ae6b5308de02a51d12eacf82cd4a9d2f7a6b599
opalenzuela <opalenzuela@gmail.com>
2014-03-04 16:12:10 +0100

    Checking available space

--- src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
@@ -18,8 +18,16 @@ then
 	echo "#ERROR You must specify the plugin ID"
 	exit 2
 else
-	echo "#INFO Installing plugin $1"
-	/usr/local/bin/plugin_add.sh $1 >/dev/null 2>/dev/null
+	SIZE=`grep ^$1 $TMPDIR/repo.lst | sort | head -n1 | cut -f7 -d';' `
+	AVAIL=`/usr/local/bin/get_mem_free.sh | cut -f1 -d' '`
+	if test "$SIZE" -gt "$AVAIL"
+	then
+		echo "#ERROR You need at least [$SIZE] Bytes free"
+		exit 3
+	else
+		echo "#INFO Installing plugin $1"
+		/usr/local/bin/plugin_add.sh $1 >/dev/null 2>/dev/null
+	fi
 	echo "list:$0"
 	echo "actions:"
 	echo "	managePlugins.sh	Back"
