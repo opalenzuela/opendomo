dea56f57a3715cd7f7554eab70d7fda0313440f8
opalenzuela <opalenzuela@gmail.com>
2014-03-05 17:58:13 +0100

    Improve rendering

--- src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
@@ -18,6 +18,8 @@ then
 	echo "#ERROR You must specify the plugin ID"
 	exit 2
 else
+	echo "#> Install plugin"
+	echo "list:$0"
 	SIZE=`grep ^$1 $REPOFILE | sort | head -n1 | cut -f7 -d';' `
 	AVAIL=`/usr/local/bin/get_mem_free.sh | cut -f1 -d' '`
 	if test "0$SIZE" -gt "0$AVAIL"
@@ -28,7 +30,7 @@ else
 		echo "#INFO Installing plugin $1"
 		/usr/local/bin/plugin_add.sh $1 >/dev/null 2>/dev/null
 	fi
-	echo "list:$0"
+	
 	echo "actions:"
 	echo "	managePlugins.sh	Back"
 fi
