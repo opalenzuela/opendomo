7bb98c26e8a05a712a4fbf9b4b32975659a98ecb
opalenzuela <opalenzuela@gmail.com>
2014-04-07 11:14:22 +0200

    ProcessRules does not belong to base system

--- src/odhal/usr/local/opendomo/eventhandlers/outputStates.sh
+++ src/odhal/usr/local/opendomo/eventhandlers/outputStates.sh
@@ -3,7 +3,12 @@
 #desc:Register output states
 #package:odhal
 
-# Copyright(c) 2013 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+
+if ! test -f /usr/local/opendomo/eventhandlers/processRules.sh
+then
+	return 1
+fi
 
 LOGFILE="/var/opendomo/log/odhal-st.h`date +%H`"
 tstamp=`date +%H:%M:%S`
