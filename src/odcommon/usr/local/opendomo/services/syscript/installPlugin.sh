baed59048230c887714363d780165858d00cb5bf
Javier Miras <jmirasb@gmail.com>
2014-04-16 15:49:07 +0200

    Fix messages in install plugin script

--- src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
@@ -9,18 +9,19 @@ PIDFILE="/var/opendomo/tmp/installplugin.pid"
 
 if ! test -f "$REPOFILE"
 then
-	echo "#ERROR You must execute Manage Plugins first"
+	echo "#ERRO You must execute Manage Plugins first"
 	exit 1
 fi
 
 if test -z "$1"
 then
-	echo "#ERROR You must specify the plugin ID"
+	echo "#ERRO You must specify the plugin ID"
 	exit 2
 else
 	if test -f "$PIDFILE"
 	then
-		echo "#ERROR System is busy. Try later"
+		echo "#ERRO System is busy. Try later"
+		echo
 		exit 3
 	fi
 	echo "#> Install plugin"
@@ -29,14 +30,15 @@ else
 	AVAIL=`/usr/local/bin/get_mem_free.sh | cut -f1 -d' '`
 	if test "0$SIZE" -gt "0$AVAIL"
 	then
-		echo "#ERROR You need at least [$SIZE] Bytes free"
+		echo "#ERRO You need at least [$SIZE] Bytes free"
 		exit 3
 	else
 		echo "#INFO Installing plugin $1"
 		/usr/local/bin/plugin_add.sh $1 &>/dev/null &
 	fi
-	
+
 	echo "actions:"
 	echo "	managePlugins.sh	Back"
 fi
 echo
+
