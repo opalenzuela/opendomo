9568d30e6b5b7c478ef993e35c6a10a9256b0bef
Javier Miras <jmirasb@gmail.com>
2014-05-25 11:47:21 +0200

    Fix save system config syntax

--- src/odcommon/usr/local/opendomo/services/syscript/saveSystemConfig.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/saveSystemConfig.sh
@@ -7,17 +7,17 @@ SYSUPDATEPID="/var/opendomo/run/updateSystem.pid"
 INSPLUGINPID="/var/opendomo/tmp/installplugin.pid"
 PIDFILE="/var/opendomo/run/saveSystemConf.pid"
 
-
 # Check process
 if test -f $PIDFILE; then
     echo "#ERRO Save config is already running"
     exit 1
+fi
 
 # Creating PID
 touch $PIDFILE
 
 # Waiting system update or plugins instalation
-while test -f $SYSUPDATEPID && test -f $INSPLUGINPID; do
+while test -f $SYSUPDATEPID || test -f $INSPLUGINPID; do
     sleep 1
 done
 
