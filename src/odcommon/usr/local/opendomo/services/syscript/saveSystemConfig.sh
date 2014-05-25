b1e0d1e27f87009db155e401679926d78435f1f5
Javier Miras <jmirasb@gmail.com>
2014-05-25 11:43:58 +0200

    Check errors in save system config

--- src/odcommon/usr/local/opendomo/services/syscript/saveSystemConfig.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/saveSystemConfig.sh
@@ -4,25 +4,32 @@
 #type:multiple
 
 SYSUPDATEPID="/var/opendomo/run/updateSystem.pid"
+INSPLUGINPID="/var/opendomo/tmp/installplugin.pid"
 PIDFILE="/var/opendomo/run/saveSystemConf.pid"
+
 
 # Check process
 if test -f $PIDFILE; then
     echo "#ERRO Save config is already running"
     exit 1
-fi
 
 # Creating PID
 touch $PIDFILE
 
-# Waiting system update in first configuration script
-while test -f $SYSUPDATEPID; do
+# Waiting system update or plugins instalation
+while test -f $SYSUPDATEPID && test -f $INSPLUGINPID; do
     sleep 1
 done
 
 echo "#LOADING Save system configuration (please wait) ..."
 echo
+if
 sudo /usr/local/sbin/mkrootfs custom >/dev/null 2>/dev/null
+then
+    echo "#INFO Configuration saved"
+else
+    echo "#ERRO Configuration can be saved"
+fi
 rm $PIDFILE
 
 /usr/local/opendomo/manageConfiguration.sh
