1f81c794a6ff7a55fadf8c7e302c03f9c6578181
Javier Miras <jmirasb@gmail.com>
2014-07-13 17:27:51 +0200

    New version of get_mem_free.sh

--- src/odcommon/usr/local/bin/get_mem_free.sh
+++ src/odcommon/usr/local/bin/get_mem_free.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-DATE=`date +%s`
-INFO=`grep MemFree /proc/meminfo| sed 's/[^0-9]//g'`
-MINFREE="1000"
-if test "$INFO" -lt "$MINFREE"; then
-	/bin/logevent warning system "Free memory under [$MINFREE]"
-fi
-echo "$DATE $INFO"
+
+# Common info
+MEMINFO="/proc/meminfo"
+PKGSTMP="/run/mounts/tmpfs"
+
+# Memory info
+TOTALMEMORY=`grep -m1 MemTotal $MEMINFO | sed 's/[^0-9]//g'`
+REALMEMFREE=`grep -m1 MemFree  $MEMINFO | sed 's/[^0-9]//g'`
+SYSTEMCACHE=`grep -m1 Cached   $MEMINFO | sed 's/[^0-9]//g'`
+
+# Changes memory info
+USEDCHNGMEM=`du $PKGSTMP 2>/dev/null | tail -n1 | awk {'print$1'}`
+let CHNGMEMTOTAL="$TOTALMEMORY / 2"
+let CHNGMEMFREE="$CHNGMEMTOTAL - $USEDCHNGMEM"
+
+# See results
+echo "TOTAL_MEMORY=$TOTALMEMORY"
+echo "REAL_MEMORY_FREE=$REALMEMFREE"
+echo "TOTAL_CACHED_MEMORY=$SYSTEMCACHE"
+echo "PACKAGE_MEMORY_TOTAL=$CHNGMEMTOTAL"
+echo "PACKAGE_MEMORY_USED=$USEDCHNGMEM"
+echo "PACKAGE_MEMORY_FREE=$CHNGMEMFREE"
