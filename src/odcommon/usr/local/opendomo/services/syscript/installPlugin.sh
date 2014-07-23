5e6b05b090ed27804ad9d4df511fe83e0fa17161
Javier Miras <jmirasb@gmail.com>
2014-07-23 17:51:40 +0200

    New installPlugins.sh version

--- src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
@@ -5,7 +5,8 @@
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 REPOFILE="/var/opendomo/tmp/repo.lst"
-PIDFILE="/var/opendomo/tmp/installplugin.pid"
+PLUGINSDIR="/var/opendomo/apt/plugins"
+QUEUEFILE="/var/opendomo/apt/queue"
 
 if ! test -f "$REPOFILE"
 then
@@ -18,28 +19,14 @@ then
 	echo "#ERRO You must specify the plugin ID"
 	exit 2
 else
-	if test -f "$PIDFILE"
-	then
-		echo "#ERRO System is busy. Try later"
-		echo
-		exit 3
-	fi
-	echo "#> Install plugin"
-	echo "list:`basename $0`"
-	SIZE=`grep ^$1 $REPOFILE | sort | head -n1 | cut -f7 -d';' `
-	AVAIL=`/usr/local/bin/get_mem_free.sh | cut -f1 -d' '`
-	if test "0$SIZE" -gt "0$AVAIL"
-	then
-		echo "#ERRO You need at least [$SIZE] Bytes free"
-		exit 3
-	else
-		echo "#INFO Installing plugin $1"
-		logevent installplugin odcommon "Installing plugin [$1]" 
-		/usr/local/bin/plugin_add.sh $1 &>/dev/null &
-	fi
+	# Sending deps to queue and download file
+	DOWNURL=`cat $REPOFILE | grep $1 | tail -n1 | cut -f2 -d";"`
+	PLUGINDEPS=`cat $REPOFILE | grep $1 | tail -n1 | cut -f4 -d";"`
+	echo -n "$PLUGINDEPS" >> $QUEUEFILE
+	cd $PLUGINSDIR
+	/usr/local/bin/download.sh $DOWNURL
 
 	echo "actions:"
 	echo "	managePlugins.sh	Back"
 fi
 echo
-
