189eeed2a86d1ae3cbe8c11e2b5bcbe0ea67fa79
opalenzuela <opalenzuela@gmail.com>
2014-08-18 16:42:39 +0200

    Some functions must be common

--- src/odcgi/var/www/scripts/common/common.js
+++ src/odcgi/var/www/scripts/common/common.js
@@ -1,3 +1,37 @@
+
+function loadJSON(filePath) {
+  // Load json file;
+  var json = loadTextFileAjaxSync(filePath, "application/json");
+  // Parse json
+  return JSON.parse(json);
+}   
+
+// Load text with Ajax synchronously: takes path to file and optional MIME type
+function loadTextFileAjaxSync(filePath, mimeType)
+{
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET",filePath,false);
+  if (mimeType != null) {
+    if (xmlhttp.overrideMimeType) {
+      xmlhttp.overrideMimeType(mimeType);
+    }
+  }
+  xmlhttp.send();
+  if (xmlhttp.status==200)
+  {
+    return xmlhttp.responseText;
+  }
+  else {
+    // TODO Throw exception
+    return null;
+  }
+}
+
+
+
+
+
+
 document.getElementsByClassName = function(cl) {
 	var retnode = [];
 	var myclass = new RegExp('\\b'+cl+'\\b');
