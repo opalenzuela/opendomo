07e7b6d9f5b590fb77559ef45d11c21876865834
opalenzuela <opalenzuela@gmail.com>
2014-08-18 17:00:44 +0200

    Adding Geopositioning

---
+++ src/odcgi/var/www/scripts/wizFirstConfigurationStep3.sh.js
@@ -0,0 +1,30 @@
+$(function(){
+	navigator.geolocation.getCurrentPosition(function(pos){
+		var lat = pos.coords.latitude;
+		var lon = pos.coords.longitude
+		var tim = new Date().getTime()/1000;
+		console.log("here I am:" + lat + " " + lon)
+		var url = "https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lon + "&timestamp=" + tim + "&sensor=false";
+		var timezone = loadJSON(url);
+		try {
+			$("#latitude").val(lat);
+			$("#longitude").val(lon);
+			$("#timezoneid").val(timezone.timeZoneId);
+			$("#timezone").val(timezone.rawOffset / 3600);
+			
+			console.log("City: " + timezone.timeZoneId);
+			console.log("TZC: " + timezone.rawOffset / 3600);
+			console.log("Timezone name: " + timezone.timeZoneName);
+			var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&sensor=false";
+		} catch (e) {
+			console.log("Error: "+e.message);
+		}
+		var location = loadJSON(url);
+		console.log ("Status: " + location.status);
+		if (location.status=="OK") {
+			console.log(location.results[0]);
+			$("#address").val(location.results[0].formatted_address);
+		}
+	});
+
+});
