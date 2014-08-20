4b9b76ae96bc021afda125c5a4cc2493199bd5a6
Javier Miras <jmirasb@gmail.com>
2014-08-20 16:30:43 +0200

    Plugins installation broken

--- src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/installPlugin.sh
@@ -12,18 +12,15 @@ if test -z "$1"; then
     echo "#ERRO You must specify the plugin ID"
     exit 2
 else
-    for plugin in $@; do
-        # Sending deps to queue and download file
-        DOWNURL=`grep $plugin $REPOFILE  | tail -n1 | cut -f2 -d";"`
-        PLUGDEPS=`grep $plugin $REPOFILE | tail -n1 | cut -f4 -d";"`
-        echo -n "$PLUGDEPS" >> $QUEUEFILE
+    # Sending deps to queue and download file
+    DOWNURL=`grep $1 $REPOFILE  | tail -n1 | cut -f2 -d";"`
+    PLUGDEPS=`grep $1 $REPOFILE | tail -n1 | cut -f4 -d";"`
+    echo -n "$PLUGDEPS" >> $QUEUEFILE
 
-        cd $PLUGINSDIR
-        /usr/local/bin/download.sh $DOWNURL
+    cd $PLUGINSDIR
+    /usr/local/bin/download.sh $DOWNURL
 
-    done
-    test -z "$2" || echo "#INFO Plugin $plugin sent to install"
-    test -z "$2" && echo "#INFO All plugins sent to install"
+    echo "#INFO Plugin $1 sent to install"
     /usr/local/opendomo/managePlugins.sh
 fi
 echo
