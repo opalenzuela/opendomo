183c7a5fa4ad0df2a5dd2deb08778121ee82a44a
opalenzuela <opalenzuela@gmail.com>
2014-08-21 09:27:42 +0200

    Minor visual adjustments

--- src/odcommon/var/www/scripts/managePlugins.sh.js
+++ src/odcommon/var/www/scripts/managePlugins.sh.js
@@ -2,7 +2,9 @@ $(function($){
 	$("li.plugin").each(function(){
 		$(this).attr("style","background-image:url('http://es.opendomo.org/files/" + $(this).attr("id") + ".png')");
 	});
-	if ($("#code")) {
-		$("#managePlugins").attr("style","background:url('http://es.opendomo.org/files/" + $("#code").val() + ".png') no-repeat top left;padding-top:100px;");
+	if ($("#code").val()!=undefined) {
+		// only when the plugin details view page is open (not in the main page)
+		$("#managePlugins").attr("style","background:url('http://es.opendomo.org/files/" + $("#code").val() + ".png') no-repeat top left;padding-top:128px;");
+		$("<iframe width='99%' height='200px' src='" + $("#webp").val() + "'></iframe>").insertAfter("#managePlugins");
 	}
 });
