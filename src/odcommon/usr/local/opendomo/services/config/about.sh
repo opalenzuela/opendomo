4eb2201377ae85583e5927d830921c5665929198
opalenzuela <opalenzuela@gmail.com>
2014-08-21 09:52:12 +0200

    fixing alignment

--- src/odcommon/usr/local/opendomo/services/config/about.sh
+++ src/odcommon/usr/local/opendomo/services/config/about.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 #desc:About
-#package:odcgi
+#package:odcommon
 #type:local
 
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 KERNELVERSION=`uname -r` 2> /dev/null
 
-if test -f /etc/VERSION; then
-	ODVERSION=`cat /etc/VERSION`
+if ! test -f /etc/os-release; then
+	echo "#ERR: Missing version information"
+	exit 0
 fi
+source /etc/os-release
 
 echo "#> About"
 echo "list:`basename $0`"
-echo "#INF: OpenDomo v.[$ODVERSION]"
+echo "#INF: OpenDomoOS v[$VERSION]"
 echo "# OpenDomo is free software, created by OpenDomo Services and the community and distributed under the GPLv3 license"
 echo "#URL:http://www.gnu.org/licenses/gpl-3.0.html"
 echo
@@ -25,10 +27,12 @@ cd /var/opendomo/plugins/
 # Checking plugins dir and see info
 if ls *; then
 	for plugin in `ls | cut -f1 -d. | uniq`; do
-		if iscfg.sh $plugin.info &>/dev/null; then
-			DESCRIPTION=`grep ^DESCRIPTION= $plugin.info | sed 's/\"//g' | cut -f2 -d= `
-			VERSION=`cat $plugin.version`
+		if iscfg.sh $plugin.info &>/dev/null; 
+		then
+			source $plugin.info 
 			echo "	-$plugin	$DESCRIPTION	package	$VERSION GLP v3"
+		else
+			echo "	-$plugin	$plugin 	package 	Unknown "
 		fi
 	done
 fi
