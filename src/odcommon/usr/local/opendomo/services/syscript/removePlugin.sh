33ae8cf84881012573b1e67e5e121ce7a26725b1
Javier Miras <jmirasb@gmail.com>
2014-08-23 12:07:49 +0200

    Delete multiple plugins remove, close issue #61

--- src/odcommon/usr/local/opendomo/services/syscript/removePlugin.sh
+++ src/odcommon/usr/local/opendomo/services/syscript/removePlugin.sh
@@ -10,11 +10,9 @@ if test -z $1; then
     echo "#ERRO You need select plugin first"
 else
     cd $PLUGINSDIR
-    for plugin in $@; do
-        if test -f $plugin.files; then
-            touch $plugin.remove
-        else
-            echo "#ERRO Plugin $plugin not installed"
-        fi
-    done
+    if test -f $PLUGIN.files; then
+        touch $PLUGIN.remove
+    else
+        echo "#ERRO Plugin $plugin not installed"
+    fi
 fi
