b8924e86d8f38775363d87eb5f47eebad11249af
opalenzuela <opalenzuela@gmail.com>
2014-08-25 15:08:27 +0200

    adding test function

--- src/odcgi/var/www/scripts/common/common.js
+++ src/odcgi/var/www/scripts/common/common.js
@@ -27,10 +27,24 @@ function loadTextFileAjaxSync(filePath, mimeType)
   }
 }
 
-
-
-
-
+/**
+ * Filter the contents of a list
+ * 
+ * This function hides all the items in a list that doesn't match the specified string
+ * 
+ * @author	Oriol Palenzuela
+ * @param listID	list's ID
+ * @param text 		string to match the selection
+ */
+function filterList(listID,text){
+	$("#" + listID+ " li").each(function(item){
+		if ($(this).text().indexOf(text)==-1){ 
+			$(this).addClass("hidden");
+		} else {
+			$(this).removeClass("hidden");
+		}
+	});
+}
 
 document.getElementsByClassName = function(cl) {
 	var retnode = [];
