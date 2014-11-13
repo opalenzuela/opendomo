691fceb17b935ead4f4ce8676bcce4f895f90934
Javier Miras Blanco <jmirasb@gmail.com>
2014-11-13 19:22:32 +0100

    Cleaning reboot.sh

--- src/odbase/usr/local/opendomo/services/config/reboot.sh
+++ src/odbase/usr/local/opendomo/services/config/reboot.sh
@@ -4,19 +4,10 @@
 #package:odbase
 #type:local
 
-SYSUPDATPID="/var/opendomo/run/updateSystem.pid"
-SAVECONFPID="/var/opendomo/run/saveSystemConf.pid"
+# TODO: Json system busy implementation
 
-if test -f $SYSUPDATPID; then
-	echo "#ERRO System update is running, system can be reboot"
-	echo
-elif test -f $SAVECONFPID; then
-	echo "#ERRO Save config is running, system can be reboot"
-	echo
-else
-	echo "# Rebooting ... Please wait"
-	echo
-	echo "actions:"
-	echo
-	/usr/local/bin/reboot.sh >/dev/null
-fi
+echo "# Rebooting ... Please wait"
+echo
+echo "actions:"
+echo
+/usr/local/bin/reboot.sh >/dev/null
