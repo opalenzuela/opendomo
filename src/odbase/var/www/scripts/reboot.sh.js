b2cdb92b58ed3eada958755f1aa285762ab57a5f
opalenzuela <opalenzuela@gmail.com>
2014-11-14 08:51:38 +0100

    First tests with soft reboot

--- src/odbase/var/www/scripts/reboot.sh.js
+++ src/odbase/var/www/scripts/reboot.sh.js
@@ -1,7 +1,17 @@
-function init_form(){
-	window.setTimeout("reloadform()",60000);
-}
+setTimeout(checkIfRestarted,5000); // First wait for 5 seconds
 
-function reloadform(){
-	window.location='/';
+function checkIfRestarted() {
+	var url = "/data/status.json";
+ 
+	$.post(url)
+	.done(function(data) {
+		//TODO: check the contents of "data"
+		console.log( "success" );
+		window.location.reload();
+	})
+	.fail(function(data) {
+		// JSON does not exist yet or connection is not available
+		console.log( "failed" );
+		setTimeout(checkIfRestarted,1000); // Then check every 2 second 
+	});
 }
