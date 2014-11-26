3178954ca075884e5a750ad5a071c02ce5d32e63
opalenzuela <opalenzuela@gmail.com>
2014-11-26 11:40:02 +0100

    Preparing for issue #102

--- src/odbase/usr/local/opendomo/services/config/manageUsers.sh
+++ src/odbase/usr/local/opendomo/services/config/manageUsers.sh
@@ -9,20 +9,19 @@
 if ! test -z $1; then
     /usr/local/opendomo/userModify.sh $1
 
-elif [ `grep -c "/bin/bash" /etc/passwd | grep -v "root:" | grep -v "admin:"` -eq 2 ]; then
-    # The list is emply
-    echo "#INFO No users configured yet"
-
 else
     # No user selected, see users list ignoring admin
     cd $CONFIGDIR
     echo "#> Available users"
     echo "list:`basename $0`    selectable iconlist"
-    IFS=$'\n'; for user in `grep "/bin/bash" /etc/passwd | grep -v "root:" | grep -v "admin:"`; do
+    for user in `grep "/bin/bash" /etc/passwd | grep -v "root:" | grep -v "admin:"`; do
         USER=`grep $user /etc/passwd | awk -F: '{print$1}'`
         INFO=`grep $user /etc/passwd | awk -F: '{print$5}'`
         echo "  -$USER  $INFO   user"
     done
+	if test -z "$USER"; then
+		echo "#INFO No users configured yet"
+	fi
     echo "action:"
     echo "      userModify.sh   Add"
     echo
