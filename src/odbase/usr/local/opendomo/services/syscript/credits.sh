2a31b8be79027624d8c5a5a2548305a4a43c9b60
opalenzuela <opalenzuela@gmail.com>
2014-12-02 17:52:46 +0100

    Credit adapted to plugins

--- src/odbase/usr/local/opendomo/services/syscript/credits.sh
+++ src/odbase/usr/local/opendomo/services/syscript/credits.sh
@@ -6,13 +6,33 @@
 
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
-echo "#> Credits"
-echo "list:credits.sh	detailed"
-echo "	dlerch	Daniel Lerch	Project founder"
-echo "	opalenzuela	Oriol Palenzuela	Lead developer"
-echo "	icatalan	Isidro Catalan	Cloud designer"
-echo "	jmirasb	Javier Miras	Software architect"
-echo "	inigoalonso	Iñigo Alonso	Logo and graphic design"
-echo "actions:"
-echo "	goback	Back"
+if test -z "$1"; then
+
+	echo "#> OpenDomoOS Credits"
+	echo "list:credits.sh	detailed"
+	echo "	dlerch  	Daniel Lerch    	user	Project founder"
+	echo "	opalenzuela	Oriol Palenzuela	user	Lead developer"
+	echo "	icatalan	Isidro Catalan  	user	Cloud designer"
+	echo "	jmirasb 	Javier Miras    	user	Software architect"
+	echo "	inigoalonso	Iñigo Alonso    	user	Logo and graphic design"
+	echo "actions:"
+	echo "	goback	Back"
+	echo
+	echo "#> Plugin credits"
+	echo "list:credits.sh	detailed"
+	cd /var/opendomo/plugins/
+	for plugin in *.info; do
+		AUTHORID=""
+		source $plugin
+		if ! test -z "$AUTHORID" && ! test -z "$AUTHOR"; then
+			echo "	$AUTHORID	$DESCRIPTION	user	$AUTHOR"
+		fi
+	done
+else
+	echo "form:credits.sh"
+	echo "	name	Name	readonly 	$1"
+	echo "	page	Page	application	https://github.com/$1"
+	echo "actions:"
+	echo "	goback	Back"
+fi
 echo
