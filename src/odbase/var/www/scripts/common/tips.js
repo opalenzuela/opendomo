3d7adc7a85e22b6d72986a0e763b6259fbca7f28
opalenzuela <opalenzuela@gmail.com>
2014-12-09 10:02:53 +0100

    Adding tips as part of the interface

---
+++ src/odbase/var/www/scripts/common/tips.js
@@ -0,0 +1,25 @@
+$(function(){
+	if (typeof localStorage.tips == "undefined") {
+		localStorage["tip:"]=[];
+	}
+	if ($("p.tip").length > 0) {
+		$("body").append("<div id='floattip'></div>");
+		$("#floattip").on("click", function(){
+			$("p.tip").show();
+			//$(this).hide();
+		});
+	}
+	$("p.tip").each(function(){
+		var t = $(this).text();
+		if (localStorage["tip:"+t] == undefined) {
+			$(this).show();
+		} else {
+			$(this).hide();
+		}
+	});
+	$("p.tip").on("click touch",function(){
+		var t = $(this).text();
+		localStorage["tip:"+t] = "checked";
+		$(this).hide();
+	});
+});
