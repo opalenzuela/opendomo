cd577900743c8738f57a17fde9d610d9adf570b1
opalenzuela <opalenzuela@gmail.com>
2014-12-15 12:07:28 +0100

    Cosmetic fixes

--- src/odbase/var/www/cgi-bin/help.sh
+++ src/odbase/var/www/cgi-bin/help.sh
@@ -4,7 +4,7 @@ echo
 lang=`cat /etc/opendomo/lang`
 TOPIC=`echo $QUERY_STRING | cut -f1 -d.`
 echo "<html><head><link rel='stylesheet' type='text/css' media='screen' href='/cgi-bin/css.cgi?admin'></head><body>"
-echo "<fieldset>"
+echo "<fieldset><p>"
 if test -f /usr/local/opendomo/docs/$TOPIC.$lang.txt; then
 	cat /usr/local/opendomo/docs/$TOPIC.$lang.txt 
 else 
@@ -15,7 +15,7 @@ else
 		i18n.sh "Manual page not found"
 	fi
 fi
-echo "</fieldset>"
+echo "</p></fieldset>"
 echo "<div class='toolbar'>"
 echo "<a class='button' href='http://www.opendomo.com/wiki/index.php?title=$TOPIC' target='_new'>"
 i18n.sh "More information"
