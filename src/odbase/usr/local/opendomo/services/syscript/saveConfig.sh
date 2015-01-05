f6af25485c7c54b22d835774d62ba24faa59a86d
Javier Miras Blanco <jmirasb@gmail.com>
2015-01-05 12:58:40 +0100

    busy state in save config scripts

--- src/odbase/usr/local/opendomo/services/syscript/saveConfig.sh
+++ src/odbase/usr/local/opendomo/services/syscript/saveConfig.sh
@@ -3,14 +3,34 @@
 #package:odbase
 #type:multiple
 
+PIDFILE="/var/opendomo/run/mkrootfs.pid"
 
-echo '{"status":"busy"}' > /var/www/data/status.json
-sudo mkrootfs
-echo '{"status":"active"}' > /var/www/data/status.json
+message() {
+    echo "#LOADING Save system configuration (please wait) ..."
+    echo
+    while  pstree | grep mkrootfs >/dev/null; do
+        sleep 1
+    done
+    echo "#INFO Your configuration was saved"
+    echo
+    echo "actions:"
+    echo "	reboot.sh	Reboot now"
+    echo
+}
 
-echo "#> Configuration saved"
-echo "#INFO Your configuration was saved"
-echo
-echo "actions:"
-echo "	reboot.sh	Reboot now"
-echo
+if   [ `cat /var/www/data/status.json | cut -f2 -d: | cut -f1 -d}` = '"busy"' ] && pstree | grep -c mkrootfs >/dev/null; then
+    # Configuration is already saving
+    message
+
+elif [ `cat /var/www/data/status.json | cut -f2 -d: | cut -f1 -d}` = '"busy"' ]; then
+    # System is busy by other process
+    echo
+    echo "#WARN Configuration can be saved yet, system is busy"
+    echo
+
+else
+    # Saving configuration
+    sudo mkrootfs &
+    sleep 1
+    message
+fi
