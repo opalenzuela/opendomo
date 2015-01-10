b599d7828e6a7fb9f756b7ad5b3fb1477cb0387d
Javier Miras Blanco <jmirasb@gmail.com>
2015-01-10 16:06:58 +0100

    Clean old and unused scripts

--- src/odbase/var/www/scripts/configureInterface.sh.js
+++
@@ -1,5 +0,0 @@
-/*
-function init_form(){
-	addToolbarButton('configureInterface',"Preview","openPopup('/cgi-bin/testcss.cgi?'+configureInterface_frm.theme.value+'+'+configureInterface_frm.skin.value);return false;","preview");
-}
-*/
--- src/odbase/var/www/scripts/configureMap.sh.js
+++
@@ -1,7 +0,0 @@
-function init_form(){
-	var im = $("#imgframe_image");
-	var theImage = new Image();
-	theImage.src = im.attr("src");	
-	$("input[name='imagw']").val(theImage.width);
-	$("input[name='imagh']").val(theImage.height);
-}
