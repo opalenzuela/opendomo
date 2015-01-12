19818f0b3375ec9145c41bc166fca4b793a65fd8
opalenzuela <opalenzuela@gmail.com>
2015-01-12 10:39:08 +0100

    Solving e-mail notify problem

--- src/odbase/usr/local/opendomo/bin/sendMailTo.sh
+++ src/odbase/usr/local/opendomo/bin/sendMailTo.sh
@@ -3,15 +3,26 @@
 #type:redundant
 #package:odbase
 
-### Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
-MAIL="$1"
+if test -z "$1"; then
+	echo "usage:$0 e-mail [Subject] Message "
+	exit 0
+fi
+
+EMAIL="$1"
 URL="http://cloud.opendomo.com/alert/index.php"
 TMPFILE="/var/opendomo/tmp/$$.tmp"
 
 TEXT=`i18n.sh "$2" | sed 's/ /+/g'`
+if ! test -z "$3"; then
+	SUBJECT="$TEXT"
+	TEXT=`i18n.sh "$3" | sed 's/ /+/g'`
+fi
+
+
 AUID=`cat /etc/opendomo/uid`
-FURL="$URL?UID=$AUID&MAIL=$EMAIL&TEXT=$TEXT"
+FURL="$URL?UID=$AUID&MAIL=$EMAIL&TEXT=$TEXT&SUBJECT=$SUBJECT"
 
 rm -fr $TMPFILE 2>/dev/null
 wget -q $FURL -O $TMPFILE 2>/dev/null
