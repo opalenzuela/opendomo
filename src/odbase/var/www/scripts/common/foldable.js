92454ac6f741870ac779ef8b81c0bff5d67c3b9a
opalenzuela <opalenzuela@gmail.com>
2015-01-22 11:51:09 +0100

    Improving foldable interaction

--- src/odbase/var/www/scripts/common/foldable.js
+++ src/odbase/var/www/scripts/common/foldable.js
@@ -1,5 +1,5 @@
 $(function($) {
-	$("fieldset.foldable").addClass("folded");
+	$("fieldset.foldable").addClass("folded").show();
 	$("fieldset.foldable").on("click",function(){
 		if ($(this).hasClass("folded")) {
 			$(this).removeClass("folded").addClass("unfolded");
