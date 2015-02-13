f465bc7a3174805c8908a42823171aaca7f1b15e
opalenzuela <opalenzuela@gmail.com>
2015-02-13 09:02:41 +0100

    Allowing reverse order eventhandlers

--- src/odbase/usr/local/opendomo/services/syscript/logEvent.sh
+++ src/odbase/usr/local/opendomo/services/syscript/logEvent.sh
@@ -3,27 +3,29 @@
 #package:odbase
 #type:local
 
-# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 EHDIR="/etc/opendomo/eventhandlers"
 
 if test -z "$1" || test -z "$2" || test -z "$3"
 then
-	echo "Usage: $0 level module message [archivo]"
-	echo "	level=error|warn|notice|debug"
-	echo "	module=odbase|odpkg|oddiscovery|any plugin or package installed"
+	echo "Usage: $0 module level message [archivo]"
+	echo "	module=odbase|any plugin installed"
+	echo "	level=error|warn|notice|debug|... (custom allowed)"
 	echo "	message=description of the event"
 	exit 0
 fi
 
-LEVEL=$1
-MODULE=$2
+MODULE=$1
+LEVEL=$2
 MSG=$3
 
 test -x $EHDIR/all-all && . $EHDIR/all-all $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/$MODULE-all && . $EHDIR/$MODULE-all $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/all-$LEVEL && . $EHDIR/all-$LEVEL  $LEVEL $MODULE "$MSG" $4
 test -x $EHDIR/$MODULE-$LEVEL && . $EHDIR/$MODULE-$LEVEL $LEVEL $MODULE "$MSG" $4
-#TODO remove the following line in future versions:
-test -x $EHDIR/odai_rules-$MODULE-$LEVEL && . $EHDIR/$MODULE-$LEVEL $LEVEL $MODULE "$MSG" $4
 
+# Tolerate inverted order as well (backwards compatibility)
+test -x $EHDIR/$LEVEL-$MODULE && . $EHDIR/$LEVEL-$MODULE $MODULE $LEVEL "$MSG" $4
+test -x $EHDIR/all-$MODULE && . $EHDIR/all-$MODULE  $MODULE $LEVEL "$MSG" $4
+test -x $EHDIR/$LEVEL-all && . $EHDIR/$LEVEL-all $MODULE $LEVEL"$MSG" $4
