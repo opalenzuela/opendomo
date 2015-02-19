946e67cc9c9a482e50df86a7e5d461bd8d01a666
opalenzuela <opalenzuela@gmail.com>
2015-02-19 10:56:05 +0100

    Several corrections for IE tiles

--- src/odbase/var/www/cgi-bin/notifications.sh
+++ src/odbase/var/www/cgi-bin/notifications.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 echo "Content-type: text/xml"
 echo
-if test -f /var/www/data/notifications.xml 
-then
-	cat /var/www/data/notifications.xml 
-fi
+echo "<tile>"
+echo "<visual lang='en-US' version='2'>"
+
+date >> /var/opendomo/tmp/notification.log
+CODE=2
+TEXT="Finalmente!"
+
+echo "<binding template='TileSquare150x150PeekImageAndText04' branding='name'>"
+echo "<image id='$CODE' src='images/logo48.png'/>"
+echo "<text id='$CODE'>$TEXT</text>"
+echo "</binding>"
+
+echo "<binding template='TileWide310x150ImageAndText01' branding='name'>"
+echo "<image id='$CODE' src='images/logo48.png'/>"
+echo "<text id='$CODE'>$TEXT</text>"
+echo "</binding>"
+
+echo "<binding template='TileSquare310x310ImageAndText01' branding='name'>"
+echo "<image id='$CODE' src='images/logo300.png'/>"
+echo "<text id='$CODE'>$TEXT</text>"
+echo "</binding>"
+
+echo "</visual>"
+echo "</tile>"
--- src/odbase/var/www/scripts/common/tips.js
+++ src/odbase/var/www/scripts/common/tips.js
@@ -1,26 +1,29 @@
 $(function(){
-	if (typeof localStorage.tips == "undefined") {
-		localStorage["tip:"]=[];
-	}
-	if ($("p.tip").length > 0) {
-		$("body").append("<div id='floattip'></div>");
-		$("#floattip").on("click", function(){
-			$("p.tip").show();
-			$(this).hide();
-		});
-	}
-	$("p.tip").each(function(){
-		var t = $(this).text();
-		if (localStorage["tip:"+t] == undefined) {
-			$(this).show();
-		} else {
-			$(this).hide();
+	try {
+		if (typeof localStorage.tips == "undefined") {
+			localStorage["tip:"]=[];
 		}
-	});
-	$("p.tip").on("click touch",function(){
-		var t = $(this).text();
-		localStorage["tip:"+t] = "checked";
-		$(this).hide();
-		$("#floattip").show();
-	});
+		if ($("p.tip").length > 0) {
+			$("body").append("<div id='floattip'></div>");
+			$("#floattip").on("click", function(){
+				$("p.tip").show();
+				$(this).hide();
+			});
+		}
+		$("p.tip").each(function(){
+			var t = $(this).text();
+			if (localStorage["tip:"+t] == undefined) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+		$("p.tip").on("click touch",function(){
+			var t = $(this).text();
+			localStorage["tip:"+t] = "checked";
+			$(this).hide();
+			$("#floattip").show();
+		});
+	} catch(e){
+	}
 });
